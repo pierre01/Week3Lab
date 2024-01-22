b040ca78aa24c8155b3ae0d69288f2e67a18dfdf
Pierre Huguet <pierre@delange.design>
2024-01-21 18:53:02 -0800

    Added Buttons

--- Week3Lab/Views/MainPage.xaml.cs
+++ Week3Lab/Views/MainPage.xaml.cs
@@ -20,8 +20,42 @@ public partial class MainPage : ContentPage
     {
         if (!string.IsNullOrEmpty(TodoTitleEntry.Text))
         {
-            _viewModel.ActiveTodos.Add(new() { Id = _todoCounter++, Title = TodoTitleEntry.Text });
+            _viewModel.ActiveTodos.Insert(0, new() { Id = _todoCounter++, Title = TodoTitleEntry.Text });
             TodoTitleEntry.Text = string.Empty;
+        }
+    }
+
+    private void OnCheckActiveTodo(object sender, EventArgs e)
+    {
+        if (_viewModel.SelectedActiveTodo != null)
+        {
+            _viewModel.CompletedTodos.Add(_viewModel.SelectedActiveTodo);
+            _viewModel.ActiveTodos.Remove(_viewModel.SelectedActiveTodo);
+        }
+    }
+
+    private void OnDeleteActiveTodo(object sender, EventArgs e)
+    {
+        if (_viewModel.SelectedActiveTodo != null)
+        {
+            _viewModel.ActiveTodos.Remove(_viewModel.SelectedActiveTodo);
+        }
+    }
+
+    private void OnCheckCompletedTodo(object sender, EventArgs e)
+    {
+        if (_viewModel.SelectedCompletedTodo != null)
+        {
+            _viewModel.ActiveTodos.Insert(0, _viewModel.SelectedCompletedTodo);
+            _viewModel.CompletedTodos.Remove(_viewModel.SelectedCompletedTodo);
+        }
+    }
+
+    private void OnDeleteCompletedTodo(object sender, EventArgs e)
+    {
+        if (_viewModel.SelectedCompletedTodo != null)
+        {
+            _viewModel.CompletedTodos.Remove(_viewModel.SelectedCompletedTodo);
         }
     }
 }
